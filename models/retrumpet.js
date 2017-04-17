b6824a50692093e55917e023eb8cfc224ce4bba5
jessethd <jessethd@gmail.com>
2017-04-17 15:39:01 +0000

    Implemented majority of trumpet API calls

--- models/retrumpet.js
+++ models/retrumpet.js
@@ -3,15 +3,7 @@ var Schema      = mongoose.Schema;
 var ObjectId	= mongoose.schema.types.ObjectId;
 
 var retrumpetSchema = new Schema({
-    trumpet: {
-        user_info_id: ObjectId,
-        reply_trumpet_id: ObjectId,
-        submit_time: { type: Data, default: Date.now},
-        text: String,
-        likes: { type: Number, default: 0},
-        retrumpets: { type: Number, default: 0},
-        replies: { type: Number, default: 0}
-    },
+    trumpet: ObjectId,
     retrumpeter_username: String
 });
         
--- routes.js
+++ routes.js
@@ -38,7 +38,91 @@ router.route('/trumpets')
          });
     });
 
-// Create a new trumpet
+// Retrieve trumpet with given ObjectID
+router.route('/trumpets/:trumpet_id')
+    .get(function(req, res) {
+        Trumpet.findById(req.params.trumpet_id, function(err, trumpet) {
+            if (err)
+                res.send(err)
+            res.json(trumpet);
+        });
+    });
+
+// Retrieve all trumpets that are replies to ObjectID (have ObjectID as reply_trumpet_id)
+
+
+// Increment the like count of a trumpet with given ObjectID by 1
+router.route('/trumpets/:trumpet_id')
+    .put(function(req, res) {
+        Trumpet.findById(req.params.trumpet_id, function(err, trumpet) {
+            if (err)
+                res.send(err);
+	    trumpet.likes = trumpet.likes + 1;
+            trumpet.save(function(err) {
+                if (err)
+                    res.send(err);
+                res.json({ message: 'Trumpet like count successfully updated.' });
+            });
+ 
+        });
+    });
+
+// Increment the retrumpet count of a trumpet with given ObjectID by 1
+router.route('/trumpets/:trumpet_id')
+    .put(function(req, res) {
+        Trumpet.findById(req.params.trumpet_id, function(err, trumpet) {
+            if (err)
+                res.send(err);
+	    trumpet.retrumpets = trumpet.retrumpets + 1;
+            trumpet.save(function(err) {
+                if (err)
+                    res.send(err);
+                res.json({ message: 'Trumpet like count successfully updated.' });
+            });
+ 
+        });
+    });
+
+
+
+// Increment the reply count of a trumpet with given ObjectID by 1
+router.route('/trumpets/:trumpet_id')
+    .put(function(req, res) {
+        Trumpet.findById(req.params.trumpet_id, function(err, trumpet) {
+            if (err)
+                res.send(err);
+	    trumpet.replies = trumpet.replies + 1;
+            trumpet.save(function(err) {
+                if (err)
+                    res.send(err);
+                res.json({ message: 'Trumpet like count successfully updated.' });
+            });
+ 
+        });
+    });
+
+
+// Create a new non-reply trumpet
+router.route('/trumpets')
+    .post(function(req, res) {
+        var trumpet = new Trumpet();
+        // Set text and user values from the request
+	trumpet.user_info_id = req.body.user_info_id;
+	trumpet.reply_trumpet_id = null;
+        trumpet.submit_time = req.body.submit_time;
+        trumpet.text = req.body.text;
+        trumpet.likes = 0;
+        trumpet.retrumpets = 0;
+        trumpet.replies = 0;
+        // Save trumpet and check for errors
+        trumpet.save(function(err) {
+            if (err)
+                res.send(err);
+            res.json({ message: 'Trumpet created and submitted successfully.' });
+        });
+    });
+
+// Create a new reply trumpet
 router.route('/trumpets')
     .post(function(req, res) {
         var trumpet = new Trumpet();
@@ -54,7 +138,7 @@ router.route('/trumpets')
         trumpet.save(function(err) {
             if (err)
                 res.send(err);
-            res.json({ message: 'Trumpet created and submitted successfully.' });
+            res.json({ message: 'Reply trumpet created and submitted successfully.' });
         });
     });
 
